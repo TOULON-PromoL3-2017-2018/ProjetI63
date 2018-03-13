f6ceb798c20e7ddd45a83dca1accaa619326d508
atobodel <alexandre-tobo-delaleu@etud.univ-tln.fr>
2018-03-13 15:59:06 +0100

    Correction table + debut remplissage

---
+++ SQL/Evenement/remplissage.sql
@@ -0,0 +1,7 @@
+--Partie remplissage Université
+INSERT INTO Universite VALUES (1, "La Garde", "La Garde",\
+                               "Rue de l'université", "13", "guigui@mail.com");
+INSERT INTO Universite VALUES (2, "ParisCentre", "Paris",\
+                               "Rue Sartre", "5", "0600000000")
+INSERT INTO Universite VALUES (3, "Aix-Marseille", "Marseille",\
+                               "Rue Pagnol", "6", "0400000000")
