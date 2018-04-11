39be26c3f9613447b080d971eca3b36cee962d39
CLAIN CYRIL <claincyril0@gmail.com>
2018-04-11 14:47:05 +0200

    commit

---
+++ SQL/MATERIEL/fill.sql
@@ -0,0 +1,17 @@
+INSERT INTO Materiel_stok VALUES
+(1,'chaussure de ski', TB, 30),
+(2,'batton de ski', TB, 30),
+(3,'combi de ski', TB, 30),
+(4,'ski', TB, 30);
+
+INSERT INTO Type_Materiel VALUES
+('hiver',1, 120),
+('montagne',2, 100),
+('route',3, 80),
+('eau',4, 30),
+('plein air',5, 60);
+
+INSERT INTO Entreprise VALUES
+(1,'DECAT','32 rue de azrea','la garde','henri',henri@henri.mail),
+(2,'go spor' ,'32 rue de zretzert','la garde','henri',marc@marc.mail),
+(3,'INTER SPORT','32 rue dezertzert','la garde','henri',titouan@titouan.mail);
