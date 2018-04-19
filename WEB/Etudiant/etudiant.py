eb245a946211e5f70d5506a26657bb01d72d3fc4
biglycan <x.richard83@free.fr>
2018-04-19 18:14:49 +0200

    Debut site étudiant, petits changements pour voyage

--- WEB/Etudiant/etudiant.py
+++ WEB/Etudiant/etudiant.py
@@ -7,35 +7,50 @@ import psycopg2
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def hello(name=None):
-    return render_template('acceuil.html', name=name)
+param = {'host' : '10.9.185.1'}
 
+try:
+    conn = psycopg2.connect(**param)
+    print("\n connecté \n")
+except:
+    print("\nERREUR DE CO !!!\n")
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('acceuil.html', name=name)
+curr = conn.cursor()
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods = ['POST','GET'])
+def hello():
+    return flask.render_template('accueil.html')
 
-def login():
-    if request.method == 'POST':
-        log()
-    else:
-        show_the_login_form()
+@app.route('/templates/form_insc_etu', methods = ['POST', 'GET'])
 
-def log():
-    user = get_user(request.form['username'])
+def new_insc():
+    return flask.render_template('form_insc_etu.html')
 
-    if user.check_password(request.form['password']):
-        login_user(user)
-        app.logger.info('%s logged in successfully', user.username)
-        return redirect(url_for('index'))
-    else:
-        app.logger.info('%s failed to log in', user.username)
-        abort(401)
+@app.route('/templates/valid_insc', methods = ['POST', 'GET'])
+
+def inscription():
+    if flask.request.method == 'POST':
+        nom_etu = flask.request.form['name_etu']
+        pre_etu = flask.request.form['pren_etu']
+        date_nais = flask.request.form['date']
+        filiere = flask.request.form['fil']
+        tel_etu = flask.request.form['tel']
+        mail_etu = flask.request.form['mail']
+        rue_etu = flask.request.form['rue']
+        ville_etu = flask.request.form['town']
+        code_post = flask.request.form['code']
+        mbr_asso = flask.request.form['suceur']
+
+        query = "INSERT INTO Etudiant(nom_etudiant, prenom_etudiant, \
+        date_naissance_etudiant, filiere_etudiant, tel_etudiant, mail_etudiant,\
+        rue_etudiant, ville_etudiant, code_postal_etudiant, membre_asso)\
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        data = (nom_etu, pre_etu, date_nais, filiere, tel_etu, mail_etu, \
+        rue_etu, ville_etu, code_post, mbr_asso)
+        curr.execute(query, data)
+        conn.commit()
+        return flask.render_template('valid_insc.html', res_nom = nom_etu)
 
 if __name__ == '__main__':
 
-    app.run()
+    app.run(debug=True)
