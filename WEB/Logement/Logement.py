4eccfa5a0b3dc165e72442c7e7d35e8d117be050
BOUTEMEUR KELLY <kelly-boutemeur@etud.univ-tln.fr>
2018-04-19 16:34:47 +0200

    Web logement mise a jour

--- WEB/Logement/Logement.py
+++ WEB/Logement/Logement.py
@@ -18,9 +18,13 @@ def result():
 
       return flask.render_template("result.html")
 
-@app.route('/emprunt',methods = ['POST', 'GET'])
-def emprunt():
-  #if flask.request.method == 'GET':
-
-  return flask.render_template("emprunt.html")
+@app.route('/new_logement',methods = ['POST', 'GET'])
+def new_logement():
+  if flask.request.method == 'POST':
+      nom="{name}".format(name=flask.request.form['name'])
+      prenom="{name}".format(name=flask.request.form['prename'])
+      adr="{name}".format(name=flask.request.form['adresse'])
+      telephone="{name}".format(name=flask.request.form['tel'])
+      #return '<form action="" method="post"><input type="text" name="name" /></form>'
+      return flask.render_template("new_logement.html",nom=nom,prenom=prenom,adr=adr,telephone=telephone)
 app.run(debug=True)
