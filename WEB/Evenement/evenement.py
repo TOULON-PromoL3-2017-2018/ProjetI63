bab68d5ad64b29c1616a9add2ba99cb385a10057
Alex9583 <none@none.none>
2018-04-21 23:27:18 +0200

    Suite de la partie WEB

--- WEB/Evenement/evenement.py
+++ WEB/Evenement/evenement.py
@@ -50,8 +50,100 @@ def inscritext():
         data = (str(NoPart), NumUniv, Nom, Prenom, Email, Telep, NumEtud)
         cur.execute(query, data)
         conn.commit()
-        return flask.render_template("inscritext.html", NoParti=NoPart)
+        return flask.render_template('inscritext.html', NoParti=NoPart)
 
-# Faire inscription interne et donner num participant
+
+@app.route("/evenement/formint/", methods=["POST", "GET"])
+def formint():
+    return flask.render_template('formint.html')
+
+
+@app.route("/evenement/inscritint/", methods=["POST", "GET"])
+def inscritint():
+    global noparticipant
+    if flask.request.method == "POST":
+        NoPart = noparticipant
+        noparticipant += 1
+        NumUniv = "1"
+        NumEtud = flask.request.form['NoEtud']
+        query = "INSERT INTO ParticipantAsso VALUES(%s, %s, %s)"
+        data = (str(NoPart), NumUniv, NumEtud)
+        cur.execute(query, data)
+        conn.commit()
+        return flask.render_template('inscritint.html', NoParti=NoPart)
+
+
+@app.route("/evenement/universite/", methods=["POST", "GET"])
+def universite():
+    query = "SELECT * FROM Universite"
+    cur.execute(query)
+    rows = cur.fetchall()
+    return flask.render_template('universite.html', Universites=rows)
+
+
+@app.route("/evenement/formuniv/", methods=["POST", "GET"])
+def formuniv():
+    return flask.render_template('formuniv.html')
+
+
+@app.route("/evenement/inscrituniv/", methods=["POST", "GET"])
+def inscrituniv():
+    if flask.request.method == "POST":
+        NomUniv = flask.request.form['NomU']
+        Ville = flask.request.form['City']
+        Rue = flask.request.form['Street']
+        Arr = flask.request.form['Arrond']
+        Cont = flask.request.form['Contact']
+        query = "INSERT INTO Universite (NomUniversite, Ville, Rue,\
+        Arrondissement, Contact) VALUES (%s, %s, %s, %s, %s)"
+        data = (NomUniv, Ville, Rue, Arr, Cont)
+        cur.execute(query, data)
+        conn.commit()
+        return flask.render_template('inscrituniv.html')
+
+
+@app.route("/evenement/equipe", methods=["POST", "GET"])
+def equipe():
+    query = "SELECT * FROM Equipe"
+    cur.execute(query)
+    rows = cur.fetchall()
+    return flask.render_template('equipe.html', Equipes=rows)
+
+
+@app.route("/evenement/formequipe/", methods=["POST", "GET"])
+def formequipe():
+    # si inscrit dans table de base ok sinon faire une erreur
+    # donc renseigner num participant pour le mettre dans inscritequipe
+    # donner num equipe comme identifiant
+    return flask.render_template('formequipe.html')
+
+
+@app.route("/evenement/inscritequipe/", methods=["POST", "GET"])
+def inscritequipe():
+    if flask.request.method == "POST":
+        NoPart = flask.request.form['NumPart']
+        NomEq = flask.request.form['NomEquipe']
+        query = "SELECT noparticipantautre FROM participantautre WHERE \
+        noparticipantautre = %s"
+        data = (NoPart)
+        cur.execute(query, data)
+        rowother = cur.fetchall()
+        if len(rowother) != 0:
+            # manque les requetes sql
+            return flask.render_template('inscritequipe.html', NomEqui=NomEq)
+        query = "SELECT noparticipantasso FROM ParticipantAsso WHERE \
+        noparticipantasso = %s"
+        data = (NoPart)
+        cur.execute(query, data)
+        rowasso = cur.fetchall()
+        if len(rowasso) != 0:
+            # manque les requetes sql
+            return flask.render_template('inscritequipe.html', NomEqui=NomEq)
+        return flask.render_template('erreurinscriteq.html', NoParti=NoPart)
+#faire les 2 html du dessus + le chemin dans equipe.html(le formpart)
+# faire import time et faire date = t.localtime() puis date[x] pour avoir les infos
+
+# demander pour compte admin sur site pour creer evenement la je fais juste
+# la participation
 
 app.run(debug=True)
